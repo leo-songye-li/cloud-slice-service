263217e7bb661020551fa4cfb97021ed4efe0edb
leo-songye-li <stpyx946@gmail.com>
2024-10-16 10:52:21 +0800

    Update start-slice.sh

--- start-slice.sh
+++ start-slice.sh
@@ -6,5 +6,5 @@ CALLBACK=$2
 SRC_FILE="1"
 echo "::add-mask::$SRC_FILE"
 SRC_FILE="234"
-# param_resp=$(curl "$CALLBACK?jobId=$JOB_ID&progress=1")
+param_resp=$(curl "$CALLBACK?jobId=$JOB_ID&progress=1")
 echo "SRC_FILE=$SRC_FILE" >> $GITHUB_OUTPUT
