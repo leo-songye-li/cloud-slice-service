fe410d1bf90740d7fff92ae38e2bc823a4e25600
leo-songye-li <stpyx946@gmail.com>
2024-10-17 17:01:58 +0800

    Update start-slice.sh

--- start-slice.sh
+++ start-slice.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
 JOB_ID=$1
 CALLBACK=$2
 
-SRC="1"
-echo "::add-mask::$SRC"
-# SRC_FILE="234"
-# param_resp=$(curl "$CALLBACK?jobId=$JOB_ID&progress=1")
-echo "SRC_FILE=$SRC" >> $GITHUB_ENV
+param_resp=$(curl -X POST -H "Content-Type: application/json" -d "{\"jobId\": \"$JOB_ID\", \"progress\": 1}" $CALLBACK)
+echo "$param_resp"
+code=$(echo $param_resp | jq -r .code)
+if [ $code -eq "200" ]
+then
+    SRC=$(echo $param_resp | jq -r .data.src)
+    echo "::add-mask::$SRC"
+    echo "SRC_FILE=$SRC" >> $GITHUB_ENV
+else
+    error=$(echo $param_resp | jq -r .msg)
+    res=$(curl -X POST -H "Content-Type: application/json" -d "{\"jobId\":\"$JOB_ID\",\"progress\":1,\"error\": \"Get src failed,CODE=$code,Msg=$error\"}" $CALLBACK)
+    exit -1
+fi
+exit 0
